083db18a1e3eeb878d5597fa42bf7fa1264f4bf6
Marcin Kowalczyk <k123443@outlook.com>
2017-03-15 04:20:05 +0100

    Symbol change

--- Matlab/rownania.m
+++ Matlab/rownania.m
@@ -1,10 +1,14 @@
-function dy = rownania(t,y,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11)
+function dx = rownania(t,x,c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11)
 
+% x1=x
+% x2=x'
+% x3=theta
+% x4=theta'
 % y = [x; x'; o; o']
-dy = zeros(4,1);
-c5 = c5*cos(y(3));
-c9 = c9*cos(y(3));
-dy(1) = y(2);
-dy(2) = c1*c6/(c5*c11) - c1*c7*c2/(c5*c5*c11)*y(2) - c1*c7*c3/(c5*c5*c11) - c1*c7*c4/(c5*c5*c11)*sin(y(3)) - c1*c8/(c5*c11)*y(2) - c1*c10/(c5*c11)*sin(y(3))*y(4)^2 + c2/c5*y(2) + c3/c5 + c4/c5*sin(y(3));
-dy(3) = y(4);
-dy(4) = c6/c11 - c7*c2/(c5*c11)*y(2) - c7*c3/(c5*c11) - c7*c4/(c5*c11)*sin(y(3)) - c8/c11*y(2) - c10/c11*sin(y(3))*y(4)^2;
+dx = zeros(4,1);
+c5 = c5*cos(x(3));
+c9 = c9*cos(x(3));
+dx(1) = x(2);
+dx(2) = c1*c6/(c5*c11) - c1*c7*c2/(c5*c5*c11)*x(2) - c1*c7*c3/(c5*c5*c11) - c1*c7*c4/(c5*c5*c11)*sin(x(3)) - c1*c8/(c5*c11)*x(2) - c1*c10/(c5*c11)*sin(x(3))*x(4)^2 + c2/c5*x(2) + c3/c5 + c4/c5*sin(x(3));
+dx(3) = x(4);
+dx(4) = c6/c11 - c7*c2/(c5*c11)*x(2) - c7*c3/(c5*c11) - c7*c4/(c5*c11)*sin(x(3)) - c8/c11*x(2) - c10/c11*sin(x(3))*x(4)^2;
